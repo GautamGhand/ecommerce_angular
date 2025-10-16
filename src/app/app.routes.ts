a3053872eba0c7c368d5768f0c200eb229c79325
GautamGhand <gautamghand820@gmail.com>
2025-10-16 18:00:01 +0530

    lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,18 +4,18 @@ import { Productview } from './productview/productview';
 import { User } from './user/user';
 
 export const routes: Routes = [
-    {
-        path:'',
-        component:Home,
-        title:'Home'
-    },
-    {
-        path:'product/show/:id',
-        component:Productview,
-    },
-    {
-        path:'users',
-        component:User,
-        title:'Users'
-    }
+  {
+    path: '',
+    loadComponent: () => import('./home/home').then((c) => c.Home),
+    title: 'Home',
+  },
+  {
+    path: 'product/show/:id',
+    loadComponent: () => import('./productview/productview').then((c) => c.Productview),
+  },
+  {
+    path: 'users',
+    loadComponent: () => import('./user/user').then((c) => c.User),
+    title: 'Users',
+  },
 ];
